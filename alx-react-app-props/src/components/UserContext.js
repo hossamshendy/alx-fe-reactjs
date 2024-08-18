209a065a9c2497c28abd18e61b90876cc9c997ae
hossamshendy <142249434+hossamshendy@users.noreply.github.com>
2024-08-18 20:07:50 +0300

    Create UserContext.js

---
+++ alx-react-app-props/src/components/UserContext.js
@@ -0,0 +1,13 @@
+import React, { createContext } from 'react';
+
+// Create a Context
+const UserContext = createContext();
+
+// Create a Provider component
+export const UserProvider = ({ children, value }) => (
+  <UserContext.Provider value={value}>
+    {children}
+  </UserContext.Provider>
+);
+
+export default UserContext;
