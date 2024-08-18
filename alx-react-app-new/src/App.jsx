b879ad131417dcafe8b8b98bd7b39ec411d5aab2
hossamshendy <142249434+hossamshendy@users.noreply.github.com>
2024-08-18 19:41:59 +0300

    Update App.jsx

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -49,5 +49,19 @@ function App() {
         </div>
     );
 }
+import React from 'react';
+import Counter from './components/Counter';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Counter Application</h1>
+      <Counter />
+    </div>
+  );
+}
+
+
+
 
 export default App;
