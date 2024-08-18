0dc5d68dfc1f9985016eba4e47b3cfa71b8096bb
hossamshendy <142249434+hossamshendy@users.noreply.github.com>
2024-08-18 19:33:14 +0300

    Create Footer.jsx

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer style={{ backgroundColor: 'gray', color: 'white', textAlign: 'center', padding: '10px' }}>
+      <p>&copy; 2024 My Favorite Cities</p>
+    </footer>
+  );
+}
+
+export default Footer;
