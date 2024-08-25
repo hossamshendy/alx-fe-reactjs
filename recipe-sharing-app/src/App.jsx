851056ae6e325c62a889d46813b134314450b0fb
hossamshendy <142249434+hossamshendy@users.noreply.github.com>
2024-08-25 16:39:04 +0300

    Update App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -31,5 +31,20 @@ function App() {
     </>
   )
 }
+import RecipeList from './components/RecipeList';
+import AddRecipeForm from './components/AddRecipeForm';
+
+function App() {
+  return (
+    <div>
+      <h1>Recipe Sharing Application</h1>
+      <AddRecipeForm />
+      <RecipeList />
+    </div>
+  );
+}
+
+
+
 
 export default App
