beff958f7272133da8f9257372d2ea50a9062cc0
hossamshendy <142249434+hossamshendy@users.noreply.github.com>
2024-09-01 16:08:01 +0300

    Update App.jsx

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -31,5 +31,23 @@ function App() {
     </>
   )
 }
+// src/App.jsx
+import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import PostsComponent from './components/PostsComponent';
+
+// Create a client
+const queryClient = new QueryClient();
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <PostsComponent />
+    </QueryClientProvider>
+  );
+}
+
+export default App;
+
 
 export default App
