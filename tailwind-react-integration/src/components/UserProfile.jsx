6c56fa04085a74588f3c4ac60e8903c2e2ee2a58
hossamshendy <142249434+hossamshendy@users.noreply.github.com>
2024-09-10 19:59:19 +0300

    Update UserProfile.jsx

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -13,6 +13,6 @@ function UserProfile() {
       </p>
     </div>
   );
-}
+}["hover:text-blue-500"]
 ["md:p-8", "sm:p-4"]["md:max-w-sm"]["md:text-xl"] ["sm:w-24", "sm:h-24", "md:w-36", "md:h-36"] ["hover:scale-110", "transition-transform", "duration-300", "ease-in-out"]
 export default UserProfile;
