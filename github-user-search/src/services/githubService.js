7c27d5592f219c1f5ddbcfe4e147c39e9058de81
hossamshendy <142249434+hossamshendy@users.noreply.github.com>
2024-09-22 15:46:24 +0300

    Update githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,1 +1,20 @@
 
+// src/services/githubService.js
+import axios from "axios";
+
+export const fetchUserData = async ({ username, location, minRepos }) => {
+  let query = "";
+
+  if (username) query += `user:${username} `;
+  if (location) query += `location:${location} `;
+  if (minRepos) query += `repos:>${minRepos} `;
+
+  try {
+    const response = await axios.get(
+      `https://api.github.com/search/users?q=${query}`
+    );
+    return response.data.items;
+  } catch (error) {
+    throw error;
+  }
+};
